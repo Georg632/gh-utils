c6dace13d4009271ef360b23fc230b53294d6ba2
Georg <geggihoe@gmail.com>
2023-01-15 21:06:53 +0100

    * routing utils

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/utils/gh-translation/gh-translation.service';
 export * from './lib/rxjs-helpers/gh-value/gh-value.directive';
 
 export * from './lib/tailwind-helpers/tw-breakpoints/tw-breakpoints.component';
+
+export * from './lib/routing/routing-helper';
---
+++ src/lib/routing/routing-helper.ts
@@ -0,0 +1,51 @@
+export const REDIRECTION: string = '**';
+export const ID_ROUTE_END: string = 'Id';
+
+export class RoutingHelper<T> {
+  private readonly PROP_PATH = '_pathProp';
+  private readonly PROP_NAVROUTE = '_navRoute';
+
+  private routingMap: T;
+
+  constructor(routingMap: T) {
+    this.routingMap = routingMap;
+    this.init();
+  }
+
+  getPath(f: (x: T) => any): string {
+    return f(this.routingMap)[this.PROP_PATH] as string;
+  }
+
+  getNavRoute(f: (x: T) => any, ...ids: string[]): string[] {
+    const pathArray = f(this.routingMap)[this.PROP_NAVROUTE] as string[];
+    if (ids && ids.length > 0) {
+      let paramCounter = 0;
+      pathArray
+        .filter((p) => p.endsWith(ID_ROUTE_END))
+        .forEach((p) => {
+          const i = pathArray.findIndex((path) => path == p);
+          pathArray.splice(i, 1, ids[paramCounter]);
+          paramCounter++;
+        });
+    }
+    return pathArray;
+  }
+
+  private init() {
+    this.addRoutingProperties(this.routingMap, ['/']);
+  }
+
+  private addRoutingProperties(obj: any, prevNavRoute: string[]) {
+    const keys = Object.keys(obj);
+    keys
+      .filter((x) => ![this.PROP_PATH, this.PROP_NAVROUTE].includes(x))
+      .forEach((key) => {
+        let o = obj[key];
+        o[this.PROP_PATH] = key.endsWith(ID_ROUTE_END)
+          ? `${prevNavRoute[prevNavRoute.length - 1]}/:${key}`
+          : key;
+        o[this.PROP_NAVROUTE] = [...prevNavRoute, key];
+        this.addRoutingProperties(o, [...prevNavRoute, key]);
+      });
+  }
+}
