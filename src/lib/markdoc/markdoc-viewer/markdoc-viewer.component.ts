588d7eff278d01a167191d1e67c7ef6f618feb47
Georg <geggihoe@gmail.com>
2023-01-18 21:54:16 +0100

    * marcdok

---
+++ src/lib/markdoc/markdoc-viewer/markdoc-viewer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'markdoc-viewer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './markdoc-viewer.component.html',
+})
+export class MarkdocViewerComponent {}
