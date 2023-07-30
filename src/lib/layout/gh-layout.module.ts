2029ce5bfa415f759c90b170ce367ec1fba49062
Georg <geggihoe@gmail.com>
2023-07-31 00:29:38 +0800

    * remove lord icon * add font awesome

--- src/lib/layout/gh-layout.module.ts
+++ src/lib/layout/gh-layout.module.ts
@@ -1,13 +1,19 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GhLayoutComponent } from './gh-layout/gh-layout.component';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { GhValueDirective } from '../rxjs-helpers/gh-value/gh-value.directive';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [GhLayoutComponent],
-  imports: [CommonModule, RouterModule, TranslateModule, GhValueDirective],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TranslateModule,
+    GhValueDirective,
+    FontAwesomeModule,
+  ],
 })
 export class GhLayoutModule {}
